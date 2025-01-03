34606e85b29f71da8af132cf9f287e110bd0e9f9
Kalyaneluri <kalyaneluri.git@gmail.com>
2025-01-03 22:40:42 +0530

    commiting Q15 changes

--- Q15.js
+++ Q15.js
@@ -1,13 +1,11 @@
 let arr=[[1,2,3],[4,5,6],[7,8,9]];
 let bag="";
-for(let i=0;i<1;i++){
-  for(let j=arr.length-1;j>=0;j--){
-    bag+=arr[j][i]+" ";
-  }
+for(let j=arr.length-1;j>=0;j--){
+    bag+=arr[j][0]+" ";
 }
 for(let i=0;i<arr.length-1;i++){
   for(let j=1;j<arr.length;j++){
-    if(i==0||i==arr.length-1||j==0||j==arr.length-1)
+    if(i==0||j==arr.length-1)
     bag+=arr[i][j]+" ";
   }
 }
